80277b09d069ec0b3ece91016fd4d8ddbac57217
Michał Szymecki <szymsonik@gmail.com>
2020-01-23 23:12:56 +0100

    create constructor

--- src/Object.java
+++ src/Object.java
@@ -3,4 +3,11 @@ public class Object {
     String producer;
     int size;
     boolean turnedOn;
+
+    Object(String n, String p, int s, boolean tO){
+        name = n;
+        producer = p;
+        size = s;
+        turnedOn = tO;
+    }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,16 +1,8 @@
 public class Room {
     public static void main(String[] args) {
-        Object object1 = new Object();
-        object1.name = "TV";
-        object1.producer = "SONY";
-        object1.size = 55;
-        object1.turnedOn = true;
+        Object object1 = new Object("TV", "SONY", 55, true);
 
-        Object object2 = new Object();
-        object2.name = "Monitor";
-        object2.producer = "LG";
-        object2.size = 27;
-        object2.turnedOn = false;
+        Object object2 = new Object("Monitor", "LG", 27, false);
 
         System.out.println("Object name: " + object1.name +
                 "\nObject producer: " + object1.producer +
